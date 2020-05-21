06dc102fc6a3515627983d22ca3d47f77ba824df
Patryk Oltuch <95oltuchpatryk@gmail.com>
2020-05-21 13:50:46 +0200

    create function for setting visibility text near planets

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const btns = [...document.querySelectorAll("a.button")];
+const planets = [...document.querySelectorAll(".planet")];
 
 btns.forEach(item => {
+
+
     item.addEventListener("click", (e) => {
-        console.log(e.target.title);
+        //console.log(e.target.title);
+        let index = 0, tmp_index = 0;
 
-        btns.forEach(item => item.classList.remove("active"));
+        btns.forEach((item, i) => {
+            // reset class
+            item.classList.remove("active");
+            planets[i].firstElementChild.classList.remove("hide-infos");
 
-        if(item.title === e.target.title){
-            item.classList.add("active");
-        }
+            if(item.title === e.target.title){
+                item.classList.add("active");
+                index = tmp_index;
+            }else{
+                tmp_index++;
+            }
+        })
+
+        planets[index].firstElementChild.classList.add("hide-infos");
     });
 })
