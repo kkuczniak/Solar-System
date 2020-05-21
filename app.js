fc75c514700638df71e629dd7f0c01330971ea68
Patryk Oltuch <95oltuchpatryk@gmail.com>
2020-05-21 14:31:48 +0200

    complete first step of application

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ btns.forEach(item => {
 
     // click on left menu
     item.addEventListener("click", (e) => {
-        //console.log(e.target.title);
         let index = 0, tmp_index = 0;
 
         btns.forEach((item, i) => {
             // reset class
             item.classList.remove("active");
             planets[i].firstElementChild.classList.remove("hide-infos");
+            planets[i].classList.remove("planet--active");
 
             if(item.title === e.target.title){
                 item.classList.add("active");
@@ -22,21 +22,32 @@ btns.forEach(item => {
         })
 
         planets[index].firstElementChild.classList.add("hide-infos");
+        planets[index].classList.add("planet--active");
     });
 })
 
 planets.forEach(item => {
 
-    //click on planet
+    // click on planets
     item.addEventListener("click", (e) => {
-        
-        planets.forEach(item => {
+        let index = 0, tmp_index = 0;
+        //console.log(e.target.dataset.name);
+
+        planets.forEach((item, i) => {
+            // reset class
+            item.firstElementChild.classList.remove("hide-infos");
             item.classList.remove("planet--active");
+            btns[i].classList.remove("active");
+
+            if(item.dataset.name === e.target.dataset.name){
+                item.firstElementChild.classList.add("hide-infos");
+                item.classList.add("planet--active");
+                index = tmp_index;
+            }else{
+                tmp_index++;
+            }
         })
 
-        if(item.title === e.target.title){
-            item.classList.add("planet--active");
-        }
-
-    })
+        btns[index].classList.add("active");
+    });
 })
